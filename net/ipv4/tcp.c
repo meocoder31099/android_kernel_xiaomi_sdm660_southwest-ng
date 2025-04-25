61cd0d6a0d5aa289de4ea28576ef20aafafd3945
duckyduckg <duckyduckg65@gmail.com>
2025-04-25 18:57:47 +0500

    BACKPORT: net: backport bbr v3 * from https://github.com/google/bbr.git * as of commit https://github.com/google/bbr/commit/cb31f3d02b1d7cd7cfdff4dd2b8b9d38879904af
    BACKPORT: net: backport bbr v3
    * from https://github.com/google/bbr.git
    * as of commit google/bbr@cb31f3d
    
    NOTE:
    1. get_random_u32_below is not available in 4.19 kernel, so using prandom_u32_max... Not the best approach.
    2. After patching, you need to edit the defconfig and add CONFIG_TCP_CONG_BBR=y below CONFIG_TCP_CONG_ADVANCED=y. For the implementation example, you can check this commit: https://github.com/kucingoranye/android_kernel_xiaomi_sdm660_419/commit/15200bf572e4aa42e43ea5e4c39c2f1c17b0250b
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>
    Signed-off-by: kucingoranye <ardiantow04@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2657,6 +2657,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 	tp->data_segs_out = 0;
 	tp->dsack_dups = 0;
 	tp->reord_seen = 0;
+	tp->fast_ack_mode = 0;
 	tp->rack.mstamp = 0;
 	tp->rack.advanced = 0;
 	tp->rack.reo_wnd_steps = 1;
@@ -3267,6 +3268,8 @@ void tcp_get_info(struct sock *sk, struct tcp_info *info)
 		info->tcpi_options |= TCPI_OPT_ECN;
 	if (tp->ecn_flags & TCP_ECN_SEEN)
 		info->tcpi_options |= TCPI_OPT_ECN_SEEN;
+	if (tp->ecn_flags & TCP_ECN_LOW)
+		info->tcpi_options |= TCPI_OPT_ECN_LOW;
 	if (tp->syn_data_acked)
 		info->tcpi_options |= TCPI_OPT_SYN_DATA;
 
