24855267dbaa0ca93a9de6322fe9b1b13376ca85
kucingoranye <ardiantow04@gmail.com>
2026-06-28 08:20:37 +0700

    kernel: fix uname bpf spoofing for root detector apps

--- kernel/sys.c
+++ kernel/sys.c
@@ -1283,6 +1283,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
+		!strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "5.4.186");
+	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	if (static_branch_likely(&susfs_is_uname_spoof_buffer_set))
 		susfs_spoof_uname(&tmp);
