6ff34cfacb433d912076324ee6451a99502c4d5e
kucingoranye <ardiantow04@gmail.com>
2026-06-28 14:45:20 +0700

    kernel: fix uname detector v2

--- kernel/sys.c
+++ kernel/sys.c
@@ -1294,16 +1294,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		susfs_spoof_uname(&tmp);
 #endif
 	up_read(&uts_sem);
-	if (copy_to_user(name, &tmp, sizeof(tmp)))
-		return -EFAULT;
-
-	if (override_release(name->release, sizeof(name->release)))
-		return -EFAULT;
-	if (override_architecture(name))
-		return -EFAULT;
-	if (override_version(name))
-		return -EFAULT;
-	return 0;
+    if (copy_to_user(name, &tmp, sizeof(tmp)))
+        return -EFAULT;
+    return 0;
 }
 
 #ifdef __ARCH_WANT_SYS_OLD_UNAME
