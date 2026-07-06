cb62dade63bac96cd059b2e1fc7683989573d33f
Vinh Nguyen <meocoder31099@icloud.com>
2026-07-06 16:41:52 +0000

    drivers: mdss_io_util: Disable spam logging "[LCD][TP][Gesture][suspend] 'xxx' power continus supply"

--- drivers/video/fbdev/msm/mdss_io_util.c
+++ drivers/video/fbdev/msm/mdss_io_util.c
@@ -347,7 +347,7 @@ int msm_dss_enable_vreg(struct dss_vreg *in_vreg, int num_vreg, int enable)
 				if( (strcmp(in_vreg[i].vreg_name,"lab")==0) ||
 						(strcmp(in_vreg[i].vreg_name,"ibb")==0) ||
 						(strcmp(in_vreg[i].vreg_name,"wqhd-vddio")==0) ) {
-					printk(KERN_ERR "[LCD][TP][Gesture][suspend] '%s' power continus supply\n",in_vreg[i].vreg_name);
+					no_printk(KERN_ERR "[LCD][TP][Gesture][suspend] '%s' power continus supply\n",in_vreg[i].vreg_name);
 					continue;
 				}
 			}
