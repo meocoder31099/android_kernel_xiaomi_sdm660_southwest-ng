919a148db96c9c22e0089a8d1c34a2955ed6928b
Vinh Nguyen <meocoder31099@icloud.com>
2026-07-06 04:02:29 +0000

    driver: nt36xxx: Debug "i2c-msm-v2 c175000.i2c: slave:0x1 is calling xfer when system is suspended"

--- drivers/i2c/busses/i2c-msm-v2.c
+++ drivers/i2c/busses/i2c-msm-v2.c
@@ -2338,9 +2338,10 @@ i2c_msm_frmwrk_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 
 	/* if system is suspended just bail out */
 	if (ctrl->pwr_state == I2C_MSM_PM_SYS_SUSPENDED) {
-		dev_dbg(ctrl->dev,
+		dev_info(ctrl->dev,
 				"slave:0x%x is calling xfer when system is suspended\n",
 				msgs->addr);
+		dump_stack();
 		return -EIO;
 	}
 
