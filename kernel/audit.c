555e8618df76044d8779afca993e64bf7e515c8d
Vinh Nguyen <meocoder31099@icloud.com>
2026-07-06 02:35:20 +0000

    kernel: audit: Disable audit spam logging

--- kernel/audit.c
+++ kernel/audit.c
@@ -1819,6 +1819,9 @@ struct audit_buffer *audit_log_start(struct audit_context *ctx, gfp_t gfp_mask,
 	struct timespec64 t;
 	unsigned int serial;
 
+	// Disable audit logging
+	return NULL;
+
 	if (audit_initialized != AUDIT_INITIALIZED)
 		return NULL;
 
